766f497d5f4f70f3c9eb5d96ffe1a94dbd6aec80
Gliscent <qkrwlsgur0625@gmail.com>
2026-02-17 22:09:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ source ~/ros2_ws/install/setup.bash
 
 export GZ_MODEL_PATH=$GZ_MODEL_PATH:$HOME/PX4-Autopilot/Tools/simulation/gz/models
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$HOME/PX4-Autopilot/Tools/simulation/gz/models:$HOME/PX4-Autopilot/Tools/simulation/gz/worlds
+export GZ_SIM_RESOURCE_PATH=$HOME/a4_models/models:$GZ_SIM_RESOURCE_PATH
 
 # 2. 기존 프로세스 정리
 echo "Cleaning up old processes..."
@@ -38,27 +39,5 @@ cd ~/PX4-Autopilot
 echo "Spawning rover 1..."
 PX4_SYS_AUTOSTART=4009 PX4_GZ_MODEL=gz_r1_rover PX4_GZ_MODEL_POSE="0,5,0,0,0,0" PX4_GZ_WORLD=test_world ./build/px4_sitl_default/bin/px4 -i 12 > /dev/null 2>&1 &
 sleep 3
-
-sleep 15
-
-# 사용 예시 (이후 삭제 가능성 고려)
-# Drone1 Arm -> Takeoff
-cd ~/PX4-Autopilot/build/px4_sitl_default/bin/
-echo "Attempting Takeoff..."
-./px4-commander --instance 10 arm
-sleep 2
-./px4-commander --instance 10 takeoff
-
-# Drone2 Arm -> Takeoff
-cd ~/PX4-Autopilot/build/px4_sitl_default/bin/
-echo "Attempting Takeoff..."
-./px4-commander --instance 11 arm
-sleep 2
-./px4-commander --instance 11 takeoff
-
-# Rover1 Arm --> offboard
-cd ~/PX4-Autopilot/build/px4_sitl_default/bin/
-./px4-commander --instance 12 arm
-./px4-commander --instance 12 mode offboard
 
 
